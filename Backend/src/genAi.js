503aaea527f02b61eaeffb7d198597118ad682b3
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2026-03-16 17:54:22 +0530

    GenAI Chatbot inside Terminal

---
+++ Backend/src/genAi.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import readline from "readline/promises";
+import { ChatMistralAI } from "@langchain/mistralai";
+import { HumanMessage, AIMessage } from "langchain";
+
+// configuration to take user input from the terminal
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// create "model" from ChatMistralAI
+const model = new ChatMistralAI({
+  model: "mistral-small-latest",
+  temperature: 0,
+});
+
+// messages array to send all chats to AI (Context simulation)
+const messages = [];
+
+while (true) {
+  const question = await rl.question("You : ");
+  messages.push(new HumanMessage(question));
+
+  const response = await model.invoke(messages);
+  messages.push(new AIMessage(response.text));
+
+  console.log("AI : ", response.text);
+}
